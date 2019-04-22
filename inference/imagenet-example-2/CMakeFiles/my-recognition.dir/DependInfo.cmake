5c2a511b6726ebaa704e665353918fa098013da2
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-04-21 22:43:23 -0700

    Added imagenet

---
+++ inference/imagenet-example-2/CMakeFiles/my-recognition.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kento/Documents/Jetson-Inference-Tutorials/imagenet-example-2/my-recognition.cpp" "/home/kento/Documents/Jetson-Inference-Tutorials/imagenet-example-2/CMakeFiles/my-recognition.dir/my-recognition.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtCore"
+  "/usr/local/cuda/include"
+  "/usr/share/qt4/mkspecs/default"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
